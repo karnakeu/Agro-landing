a8738854ea17dbfee95e6f1212aabfd6ec1beeed
karnakeu <n58077624@gmail.com>
2025-03-12 16:31:44 +0500

    fonts and few changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="red font-covered">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
