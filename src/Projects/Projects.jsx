80032d5a7cc19a6032acf12f8e7ce072727f3ad5
karnakeu <n58077624@gmail.com>
2025-03-17 18:19:41 +0500

    some changes project

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -8,33 +8,34 @@ import a5 from "../../src/assets-amelie/a5.png";
 import a6 from "../../src/assets-amelie/a6.png";
 
 const Projects = () => {
-    const items = [
-        { title: "Agriculture Farming", image: a1 },
-        { title: "Ecological Farming", image: a2 },
-        { title: "Organic Solutions", image: a3 },
-        { title: "Fresh Products", image: a4 },
-        { title: "Healthy Food", image: a5 },
-        { title: "Easy Harvesting", image: a6 },
-      ];
+  const items = [
+    { title: "Agriculture Farming", image: a1 },
+    { title: "Ecological Farming", image: a2 },
+    { title: "Organic Solutions", image: a3 },
+    { title: "Fresh Products", image: a4 },
+    { title: "Healthy Food", image: a5 },
+    { title: "Easy Harvesting", image: a6 },
+  ];
 
-    return (
-        <div className="grid grid-cols-3 gap-4 p-4 w-[1170px] mt-[450px] mx-[120px]">
-            {items.map((item, index) => (
-                <div
-                key={index}
-                className={`relative w-[370px] h-[570px] bg-cover bg-center rounded-xl shadow-lg`}
-                style={{ backgroundImage: `url(${item.image})` }}
-                >
-                {item.title && (
-                    <div className="absolute w-[168px] bottom-4 left-4  bg-opacity-50 white text-3xl  text-left font-manrope-extrabold p-2 rounded">
-                    {item.title}
-                    </div>
-                )}
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center min-h-screen mt-[450px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 p-4 max-w-[1170px]">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="relative w-[370px] h-[570px] bg-cover bg-center rounded-xl shadow-lg"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            {item.title && (
+              <div className="absolute w-[168px] bottom-4 left-4 bg-opacity-50 text-white text-3xl font-manrope-extrabold p-2 rounded">
+                {item.title}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-
-export default Projects
+export default Projects;
